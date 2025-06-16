df66c003caa2e58e3a90a4f5724fd34e353b0c55
Wenard <wenardken57@gmail.com>
2025-06-16 22:06:05 +0800

    Add navigation style and website title

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,11 @@
+import "./../style/Navigation.css";
+
 function Navigation() {
-  return <div className="navigation">Navigation</div>;
+  return (
+    <div className="navigation">
+      <h1>Not to-do</h1>
+    </div>
+  );
 }
 
 export default Navigation;
