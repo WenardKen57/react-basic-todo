e61f412d4ecc65f3dd480ca30138f0c9aa6622d6
Wenard <wenardken57@gmail.com>
2025-06-16 21:46:08 +0800

    Add general layout of website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import LeftPanel from "./components/LeftPanel";
+import Navigation from "./components/Navigation";
+import RightPanel from "./components/RightPanel";
+
+import "./style/App.css";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="container">
+      <Navigation></Navigation>
+      <LeftPanel></LeftPanel>
+      <RightPanel></RightPanel>
+    </div>
+  );
 }
 
 export default App;
