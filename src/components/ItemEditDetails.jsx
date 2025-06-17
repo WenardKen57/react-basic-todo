2cebd9f07fd35f6df3774b1ed9ba52df4368521b
Wenard <wenardken57@gmail.com>
2025-06-17 19:34:38 +0800

    remove formatDate function

--- src/components/ItemEditDetails.jsx
+++ src/components/ItemEditDetails.jsx
@@ -12,13 +12,6 @@ function ItemEditDetails({ list, setList, itemID }) {
     setList(updatedList);
   }
 
-  function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
-    const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
-  }
-
   return (
     <form action="">
       <label htmlFor="itemTitle">(What you don't want to do):</label>
